72086f1e39bf7c11497aeceb58066692f9e8f294
BenLewis <benlewisj@gmail.com>
2015-05-04 18:36:29 +1200

    Added map API

--- src/main/java/com/skinny121/Map.java
+++ src/main/java/com/skinny121/Map.java
@@ -4,16 +4,123 @@ import org.mapdb.DB;
 import org.mapdb.DBMaker;
 
 import java.io.File;
+import java.security.PolicySpi;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentNavigableMap;
 
 public class Map {
     private final DB database;
+    private ConcurrentNavigableMap<Long, byte[]> chunks;
+    private ConcurrentNavigableMap<Long, byte[]> cols;
+
 
     public Map(String filename){
         database = DBMaker.newFileDB(new File(filename))
                 .closeOnJvmShutdown().make();
+        chunks = database.getTreeMap("chunks");
+        cols = database.getTreeMap("columns");
+    }
+
+    /*
+        This method is from the tall worlds source.
+        https://github.com/TallWorlds/CubicChunks/blob/develop/src/main/cubicchunks/util/Bits.java
+     */
+    private static int unpackSigned(long packed, int size, int offset) {
+        // first, offset to the far left and back so we can preserve the two's complement
+        int complementOffset = 64 - offset - size;
+        packed = packed << complementOffset >> complementOffset;
+
+        // then unpack the integer
+        packed = packed >> offset;
+        return (int)packed;
+    }
+
+    private long getAddress(int x, int z){
+        return getAddress(x, 0, z);
+    }
+
+    private long getAddress(int x, int y, int z){
+        x = x & ((1<<22)-1);
+        z = z & ((1<<22)-1);
+        y = y & ((1<<20)-1);
+        return ((long)y << 44) | ((long)x << 22) | ((long)z);
+    }
+
+    private int getX(long address){
+        return unpackSigned(address, 22, 22);
+    }
+
+    private int getZ(long address){
+        return unpackSigned(address, 22, 0);
+    }
+
+    private int getY(long address){
+        return unpackSigned(address, 20, 44);
+    }
+
+    public byte[] getChunk(int x, int y, int z){
+        return chunks.get(getAddress(x, y, z));
+    }
+
+    public List<Pos> getChunks(){
+        List<Pos> poss = new ArrayList<>();
+        for(long address:chunks.keySet()){
+            poss.add(new Pos(address));
+        }
+        return poss;
+    }
+
+    public boolean containsChunk(int x, int y, int z){
+        return chunks.containsKey(getAddress(x, y, z));
+    }
+
+    public void saveChunk(int x, int y, int z, byte[] data){
+        chunks.put(getAddress(x, y, z), data);
+    }
+
+    public byte[] getColumn(int x, int z){
+        return cols.get(getAddress(x, z));
+    }
+
+    public List<Pos> getColumns(){
+        List<Pos> poss = new ArrayList<>();
+        for(long address:cols.keySet()){
+            poss.add(new Pos(address));
+        }
+        return poss;
+    }
+
+    public boolean containsColumn(int x, int z){
+        return cols.containsKey(getAddress(x, z));
+    }
+
+    public void saveColumn(int x, int z, byte[] data){
+        cols.put(getAddress(x, z), data);
     }
 
     public void close(){
         database.close();
+    }
+
+    public class Pos{
+        public final int x;
+        public final int y;
+        public final int z;
+
+        private Pos(long address){
+            this(getX(address), getY(address), getZ(address));
+        }
+
+        public Pos(int x, int y, int z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        @Override
+        public String toString() {
+            return "("+x+","+y+","+z+")";
+        }
     }
 }
---
+++ src/test/java/com/skinny121/MapTest.java
@@ -0,0 +1,54 @@
+package com.skinny121;
+
+import com.skinny121.nbt.*;
+import org.junit.*;
+
+import java.io.ByteArrayInputStream;
+
+public class MapTest {
+    private Map map;
+    @Before
+    public void setUp(){
+        map = new Map("/Users/BenLewis/Desktop/TallWorlds/saves/New World/cubes.dim0.db");
+    }
+
+    @After
+    public void tearDown(){
+        map.close();
+    }
+
+    @Test
+    public void chunkTest(){
+        System.out.println(map.getChunks());
+        System.out.println(map.getChunks().size());
+    }
+
+    @Test
+    public void chunkData(){
+        byte[] data = map.getChunk(-7, -4, -6);
+        CompoundTag t = (CompoundTag)Importer.importNBT(new ByteArrayInputStream(data));
+
+        Assert.assertEquals(t.getTag("x"), new IntTag("x", -7));
+        Assert.assertEquals(t.getTag("y"), new IntTag("y", -4));
+        Assert.assertEquals(t.getTag("z"), new IntTag("z", -6));
+
+        System.out.println(NBTToJson.toJson(t, new NBTToJson.Options(true)));
+    }
+
+    @Test
+    public void columnTest(){
+        System.out.println(map.getColumns());
+        System.out.println(map.getColumns().size());
+    }
+
+    @Test
+    public void columnData(){
+        byte[] data = map.getColumn(12, 11);
+        CompoundTag t = (CompoundTag)Importer.importNBT(new ByteArrayInputStream(data));
+
+        Assert.assertEquals(t.getTag("x"), new IntTag("x", 12));
+        Assert.assertEquals(t.getTag("z"), new IntTag("z", 11));
+
+        System.out.println(NBTToJson.toJson(t, new NBTToJson.Options(true)));
+    }
+}
