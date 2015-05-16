7f7e0b922fbc72ad931ca23c489e9aa06107c6e8
BenLewis <benlewisj@gmail.com>
2015-05-17 11:42:25 +1200

    Added log4j logging and added license

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tall World map server
+
+This application reads and writes the chunk format for the [Minecraft](http://www.minecraft.net/)
+mod [Tall Worlds](http://www.cuchazinteractive.com/tall-worlds/).
+
+This is used to communicate to other applications using a defined protocol.
--- src/main/java/com/skinny121/Connection.java
+++ src/main/java/com/skinny121/Connection.java
@@ -1,3 +1,27 @@
+/*
+Copyright (c) 2015, Ben Lewis
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.skinny121;
 
 import com.google.common.base.Throwables;
@@ -189,6 +213,7 @@ public class Connection {
         int len;
         int[] result;
         if(chunk){
+            logger.info("Received packet list chunks");
             List<Map.Pos> chunks = map.getChunks();
             len = chunks.size();
             result = new int[3*len];
@@ -198,6 +223,7 @@ public class Connection {
                 result[3*i+2] = chunks.get(i).z;
             }
         }else{
+            logger.info("Received packet list columns");
             List<Map.Pos> columns = map.getColumns();
             len = columns.size();
             result = new int[2*len];
